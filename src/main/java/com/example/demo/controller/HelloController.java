f92d1771dad5bdf1a160e73b4c368be45c54eeff
zhangy <Administrator@PC-201907300935>
2020-03-25 16:02:00 +0800

    ddd

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -5,4 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }

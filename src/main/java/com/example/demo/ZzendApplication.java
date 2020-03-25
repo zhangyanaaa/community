14faa850cd3ea3b6eddb94316a773f03a84fe2e5
zhangy <Administrator@PC-201907300935>
2020-03-25 15:03:39 +0800

    001
    Signed-off-by: zhangy <Administrator@PC-201907300935>

---
+++ src/main/java/com/example/demo/ZzendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/ZzendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

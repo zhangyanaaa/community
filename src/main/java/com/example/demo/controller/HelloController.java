1208e79c1fd53815f16f9eb5260d7ba20b521a4c
zhangy <Administrator@PC-201907300935>
2020-03-25 15:50:49 +0800

    test22

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
-public class HelloController {
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class HelloController {
+	
 }
